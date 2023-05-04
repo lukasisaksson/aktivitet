89f4335ebb1a4fe9d754dbfe6ede3c6991a37a12
lukasisaksson <116803275+lukasisaksson@users.noreply.github.com>
2023-05-04 12:10:45 +0200

    JAVASCRIPT FUNGERAR + Kommentering

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+/*javascript kod för nedscrollning av boxarna*/
+const boxes = document.querySelectorAll('.box'); /*Difenierar vad som ska döljas/visas (Klassen box) */
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 4; 
+
+  boxes.forEach((box) => { /* Om box är över en viss höjd kommer den dyka upp, annars är den placerad vid sidan*/
+    const boxTop = box.getBoundingClientRect().top;
+     
+      if (boxTop < triggerBottom){
+        box.classList.add('show');
+      } else{
+        box.classList.remove('show');
+      }
+   });
+}
+
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
@@ -9,7 +31,7 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
-
+/*Javascript kod för slideshow*/
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -19,13 +41,13 @@ function plusSlides(n) {
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
-}
+} /*Difenierar vilken slide som ska flyttas eller vilken man ska bytta till*/
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}  
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
